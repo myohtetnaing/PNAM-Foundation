52308146cf776ae1b9d374421e7b4b267d0a7e3c
myohtetnaing <myohtetnaing.dev@gmail.com>
2024-04-16 21:54:03 +0630

    PNAM Foundation Project

---
+++ Js/(2019-2020).js
@@ -0,0 +1,30 @@
+
+
+
+	
+	//Tin Tin Tun
+	
+	var ttt_jan_2019_2020 = document.getElementById('ttt_jan_2019_2020').textContent=1000;
+	var ttt_feb_2019_2020 = document.getElementById('ttt_feb_2019_2020').textContent=1000;
+	var ttt_mar_2019_2020 = document.getElementById('ttt_mar_2019_2020').textContent=1000;
+	var ttt_apr_2019_2020 = document.getElementById('ttt_apr_2019_2020').textContent=1000;
+	var ttt_may_2019_2020 = document.getElementById('ttt_may_2019_2020').textContent=1000;
+	var ttt_jun_2019_2020 = document.getElementById('ttt_jun_2019_2020').textContent=1000;
+	var ttt_jul_2019_2020 = document.getElementById('ttt_jul_2019_2020').textContent=1000;
+	var ttt_aug_2019_2020 = document.getElementById('ttt_aug_2019_2020').textContent=1000;
+	var ttt_sep_2019_2020 = document.getElementById('ttt_sep_2019_2020').textContent=1000;
+	var ttt_nov_2019_2020 = document.getElementById('ttt_nov_2019_2020').textContent=1000;
+	var ttt_oct_2019_2020 = document.getElementById('ttt_oct_2019_2020').textContent=1000;
+	var ttt_dec_2019_2020 = document.getElementById('ttt_dec_2019_2020').textContent=1000;
+	var ttt_total_2019_2020 = document.getElementById('ttt_total_2019_2020');
+	var ttt_final_total_2019_2020 = ttt_total_2019_2020.innerHTML = ttt_jan_2019_2020+ttt_feb_2019_2020+ttt_mar_2019_2020+ttt_apr_2019_2020+ttt_may_2019_2020+ttt_jun_2019_2020+ttt_jul_2019_2020+ttt_aug_2019_2020+ttt_sep_2019_2020+ttt_nov_2019_2020+ttt_oct_2019_2020+ttt_dec_2019_2020;
+
+// For Final Total
+	var final_total_2019_2020 = document.getElementById('final_total_2019_2020');
+	var for_final_result_2019_2020 = final_total_2019_2020.innerHTML = ttt_final_total_2019_2020;
+
+// For All Final Total
+	var all_final_total_2019_2020 = document.getElementById('all_final_total_2019_2020');
+	all_final_total_2019_2020.innerHTML = for_final_result_2019_2020;
+
+	
